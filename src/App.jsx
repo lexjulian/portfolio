fb9e6a997ee2100abe1eb90534ffd4e9ea135a39
lexjulian <alexanderjohnjulian@gmail.com>
2024-10-09 15:38:14 +0800

    added theme provider and new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,28 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
-import ProjectDetail from "./pages/ProjectDetail"
-import { Link } from "react-router-dom";
+import Projects from "./pages/Projects";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import "./styles/globals.css";
+import Navbar from "./components/ui/Navbar";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return (
-    <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/projectdetail">ProjectDetail</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projectdetail" element={<ProjectDetail />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Router>
+               <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    );
 }
 
 export default App;
